3998e990d4a0f4ce6e7cc3934d02455bd04e23a4
Emir Hadzimahmutovic <emcapentest@gmail.com>
2022-03-31 22:43:29 +0200

    remove env file

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,6 @@ import server from './core/server';
 import container from './core/container';
 import TYPES from './core/types';
 import { Logger } from './core/logger';
-import AWS from './core/aws';
-
-const getPassword = async () => {
-  const ssm = new AWS.SSM();
-  const options = {
-    Name: '/dev/ninjadev/password/master',
-    WithDecryption: true,
-  };
-  const result: AWS.SSM.GetParameterResult = await ssm.getParameter(options).promise();
-  console.log(result);
-  return result.Parameter;
-};
-
-getPassword();
 
 const logger: Logger = container.get<Logger>(TYPES.Logger);
 
